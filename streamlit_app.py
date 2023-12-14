8bfb6bdd991afca454cbbf1cb26011a69770f906
Martin Pintér <74717348+PMrtn@users.noreply.github.com>
2023-12-14 07:17:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,39 +2,46 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import joblib
 
-"""
-# Welcome to Streamlit!
+RNN_model = joblib.load('trueskill_sigma.sav')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+num_points = st.slider("GPM", 1, 10000, 1100)
+num_turns = st.slider("XPM", 1, 300, 31)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if(st.session_state.bt and st.session_state.pred is False):
+    with col1:
+        col1_data = st.number_input('GPM', min_value=0, value=0, step=1)
+    with col2:
+        col2_data = st.number_input('XPM', min_value=0, value=0, step=1)
+    with col3:
+        col3_data = st.number_input('Kills', min_value=0, value=0, step=1)
+    with col4:
+        col4_data = st.number_input('Assists', min_value=0, value=0, step=1)
+    with col5:
+        col5_data = st.number_input('Last hits', min_value=0, value=0, step=1)
+    with col6:
+        col6_data = st.number_input('Hero damage', min_value=0, value=0, step=1)
+    with col7:
+        col7_data = st.number_input('Tower damage', min_value=0, value=0, step=1)
+    with col8:
+        col8_data = st.number_input('XP from hero kills', min_value=0, value=0, step=1)
+    with col9:
+        col9_data = st.number_input('XP from creeps', min_value=0, value=0, step=1)
+    with col10:
+        col10_data = st.number_input('Other XP', min_value=0, value=0, step=1)
+    with col11:
+        col11_data = st.number_input('Gold for killing heros', min_value=0, value=0, step=1)
+    with col12:
+        col12_data = st.number_input('Gold for killing creeps', min_value=0, value=0, step=1)
+    with col13:
+        col13_data = st.number_input('Total matches', min_value=0, value=0, step=1)
+    with col14:
+        col14_data = st.number_input('Total wins', min_value=0, value=0, step=1)
+        
+  
+    PredictionButton = st.button('Predict trueskill_sigma!')
+    if(PredictionButton):
+        prediction = RNN_model.predict([[col1_data,col2_data,col3_data,col4_data,col5_data,col6_data,col7_data,col8_data,col9_data,col10_data,col11_data,col12_data,col13_data,col14_data]])
+        saveResult(prediction)
+        pred()
