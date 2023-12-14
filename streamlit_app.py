1c6215796a897641b4d991ae2b8889631590482e
Martin Pintér <74717348+PMrtn@users.noreply.github.com>
2023-12-14 07:51:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,25 @@ import streamlit as st
 import tensorflow as tf
 #import joblib
 
+st.write("""
+         Trueskill_sigma regression
+         Pintér Martin
+         """)
 
 #RNN_model = joblib.load('trueskill_sigma.sav')
 
-gpm = st.slider('GPM', min_value=0, max_value=999, value=0, step=1)
-xpm = st.slider('XPM', min_value=0, max_value=999, value=0, step=1)
-kills = st.slider('Kills', min_value=0, max_value=999, value=0, step=1)
-assists = st.slider('Assists', min_value=0, max_value=999, value=0, step=1)
-last_hits = st.slider('Last hits', min_value=0, max_value=999, value=0, step=1)
-hero_damage = st.slider('Hero damage', min_value=0, max_value=999, value=0, step=1)
-tower_damage = st.slider('Tower damage', min_value=0, max_value=999, value=0, step=1)
-xp_hero_kills = st.slider('XP from hero kills', min_value=0, max_value=999, value=0, step=1)
+gpm = st.slider('GPM', min_value=0, max_value=1200, value=650, step=1)
+xpm = st.slider('XPM', min_value=0, max_value=1200, value=700, step=1)
+kills = st.slider('Kills', min_value=0, max_value=100, value=10, step=1)
+deaths = st.slider('Deaths', min_value=0, max_value=100, value=4, step=1)
+assists = st.slider('Assists', min_value=0, max_value=100, value=16, step=1)
+last_hits = st.slider('Last hits', min_value=0, max_value=1500, value=642, step=1)
+hero_damage = st.slider('Hero damage', min_value=0, max_value=150000, value=68469, step=1)
+tower_damage = st.slider('Tower damage', min_value=0, max_value=30000, value=12467, step=1)
+xp_hero_kills = st.slider('XP from hero kills', min_value=0, max_value=20000, value=0, step=1)
 xp_creeps = st.slider('XP from creeps', min_value=0, max_value=999, value=0, step=1)
 other_xp = st.slider('Other XP', min_value=0, max_value=999, value=0, step=1)
-gold_killing_heros = st.slider('Gold for killing heros', min_value=0, max_value=999, value=0, step=1)
+gold_killing_heroes = st.slider('Gold for killing heroes', min_value=0, max_value=999, value=0, step=1)
 gold_killing_creeps = st.slider('Gold for killing creeps', min_value=0, max_value=999, value=0, step=1)
 total_matches = st.slider('Total matches', min_value=0, max_value=999, value=0, step=1)
 total_wins = st.slider('Total wins', min_value=0, max_value=999, value=0, step=1)
@@ -27,6 +32,8 @@ total_wins = st.slider('Total wins', min_value=0, max_value=999, value=0, step=1
         
 PredictionButton = st.button('Predict trueskill_sigma!')
 if(PredictionButton):
-    prediction = RNN_model.predict([[col1_data,col2_data,col3_data,col4_data,col5_data,col6_data,col7_data,col8_data,col9_data,col10_data,col11_data,col12_data,col13_data,col14_data]])
+    prediction = RNN_model.predict([[gpm,xpm,kills,deaths,assists,last_hits,hero_damage,tower_damage,xp_hero_kills,xp_creeps,other_xp,gold_killing_heroes,gold_killing_creeps,total_matches,total_wins]])
     saveResult(prediction)
     pred()
+
+st.subheader(f'The predicted trueskill_sigma is: {[prediction[0]]}')
